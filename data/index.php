d7abc002709dacd134874c2af3220c14cb98976a
Richard Tape <richard@iamfriendly.com>
2016-03-23 11:36:38 -0700

    ADDED mariadb connections ADDED data volume for the app ADDED quick test for the database connection in the PHP file

--- data/index.php
+++ data/index.php
@@ -1,2 +1,21 @@
 <?php echo phpversion(); ?>
-<?php echo ' -> Test'; ?>
+
+<?php
+
+$mysqli = new mysqli( 'mariadb', 'root', '123456', 'myapp' );
+
+if ( $mysqli->connect_errno ) {
+
+	echo 'Sorry, this website is experiencing problems.';
+
+	// Something you should not do on a public site, but this example will show you
+	// anyways, is print out MySQL error related information -- you might log this
+	echo 'Error: Failed to make a MySQL connection, here is why: <br />';
+	echo 'Errno: ' . absint( $mysqli->connect_errno ) . '<br />';
+	echo 'Error: ' . $mysqli->connect_error . '<br />';
+
+	// You might want to show them something nice, but we will simply exit
+	exit;
+}
+
+$mysqli->close();
