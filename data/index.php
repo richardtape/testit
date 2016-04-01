f537b2594114de82c7a57ee48accd0b381c02671
Richard Tape <richard@iamfriendly.com>
2016-04-01 15:13:06 -0700

    CHANGED template file to look for all urls from the root. REMOVED index.html file so the index.php takes precedence CHANGED index.php file to better show what's going on

--- data/index.php
+++ data/index.php
@@ -1,7 +1,5 @@
-<?php echo 'Hi! This is PHP Version: ' . phpversion(); ?>
-
 <?php
-
+/*
 $mysqli = new mysqli( 'mariadb', 'root', '123456', 'myapp' );
 
 if ( $mysqli->connect_errno ) {
@@ -19,3 +17,12 @@ if ( $mysqli->connect_errno ) {
 }
 
 $mysqli->close();
+*/
+?>
+<html>
+<body>
+	<h1>Hi there!</h1>
+	<p><?php echo 'Hi! This is PHP Version: ' . phpversion(); ?></p>
+	<p>To test things, <a id="go-to-other" href="/other.php">here is another page</a>.</p>
+</body>
+</html>
---
+++ data/other.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+	<h1>Other!</h1>
+	<p>You can <a href="/index.php">go back to the index</a>.</p>
+</body>
+</html>
